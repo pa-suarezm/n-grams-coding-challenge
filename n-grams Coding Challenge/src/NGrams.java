10be8aee6107b94b6be05dfcc8bf6282452b151b
pa-suarezm <pabloasuarezm@hotmail.com>
2020-10-09 14:59:14 -0500

    Almost everything done. Missing algorithms and printing results to console

--- n-grams Coding Challenge/src/NGrams.java
+++ n-grams Coding Challenge/src/NGrams.java
@@ -6,26 +6,30 @@
  */
 
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class NGrams
 {
-	
+
 	/**
 	 * Main method. Will take user input using the console and output through there also.
 	 * @param args
 	 */
 	public static void main(String[] args)
 	{
-		Scanner s = new Scanner(System.in);			//Takes user input by reading from console (a.k.a. System.in)
+		Scanner s = new Scanner(System.in).useDelimiter("\n"); //Takes user input by reading from console (a.k.a. System.in)
+
+		//Variables to be used on each iteration
 		int menuOption = 0;
 		String userInput = "";
-		
+
 		while(0 <= menuOption && menuOption <= 2) //Quits if the selected option is 3
 		{
 			printMenu();
-			
+
 			userInput = s.next();
-			
+
 			try
 			{
 				menuOption = Integer.parseInt(userInput);
@@ -35,7 +39,7 @@ public class NGrams
 				printInvalidInput();
 				continue;
 			}
-			
+
 			if(menuOption != 1 || menuOption != 2 || menuOption != 3)
 			{
 				printInvalidInput();
@@ -44,47 +48,113 @@ public class NGrams
 			{
 				//Runs the program if the selected menu option is valid
 				if(menuOption == 3)	//Program ends
+				{
 					break;
+				}
 				else if(menuOption == 1) //calculateNGrams
 				{
+					String msg =  "------------------------------------------\n"
+							+ "Selected calculateNGrams.\n"
+							+ "Please input the text to be analyzed:\n";
+					System.out.println(msg);
+
+					String text = s.next();
+
+					msg = "Please input the desired n-gram to be calculated:";
+					System.out.println(msg);
+
+					int n = -1;
+					try
+					{
+						n = Integer.parseInt(s.next());
+					}
+					catch(Exception e)
+					{
+						printInvalidParams();
+						continue;
+					}
+
+					List<String> ans = calculateNGrams(text, n);
 					
+					//TODO print results
 				}
 				else //mostFrequentNGram
 				{
+					String msg =  "------------------------------------------\n"
+							+ "Selected mostFrequentNGram.\n"
+							+ "Please input the text to be analyzed:\n";
+					System.out.println(msg);
+
+					String text = s.next();
+
+					msg = "Please input the desired n-gram to be evaluated:";
+					System.out.println(msg);
+
+					int n = -1;
+					try
+					{
+						n = Integer.parseInt(s.next());
+					}
+					catch(Exception e)
+					{
+						printInvalidParams();
+						continue;
+					}
+
+					String ans = mostFrequentNGram(text, n);
 					
+					//TODO print results
 				}
 			}
 		}
+
+		s.close();
 	}
 
-	private static void calculateNGrams(String text, int n)
+	/**
+	 * 
+	 * @param text: String to be analyzed
+	 * @param n: Indicates the n to be used in calculating the n-grams
+	 * @return ans: The whole list of n-grams for the text
+	 */
+	private static List<String> calculateNGrams(String text, int n)
 	{
-		
+		List<String> ans = new ArrayList<String>();
+		//TODO algorithm
+		return ans;
 	}
-	
-	private static void mostFrequentNGram (String text, int n)
+
+	/**
+	 * 
+	 * @param text: String to be analyzed
+	 * @param n: Indicates the n to be used in calculating the n-grams
+	 * @return ans: The most frequent n-gram found in the calculated n-grams
+	 */
+	private static String mostFrequentNGram (String text, int n)
 	{
-		
+		String ans = "";
+		//TODO algorithm
+		return ans;
 	}
-	
+
 	/**
 	 * Prints the menu the user will interact with
 	 */
 	private static void printMenu()
 	{
 		String menu = "====================================================\n"
-					+ "-----------------------N-grams----------------------\n"
-					+ "====================================================\n\n"
-					+ "Please press the number for the function you want and 'enter'\n"
-					+ "You'll be prompted to enter the needed parameters for each function.\n\n"
-					+ "1. calculateNGrams: Will ask for a text and a whole number n, and then will return all n-grams for\n"
-					+ "that text\n"
-					+ "2. mostFrequentNGram: Will ask for a text and a whole number n, and then will return only the most\n"
-					+ "frequent item of the n-grams for that text\n"
-					+ "3. Exit program";
+				+ "-----------------------N-grams----------------------\n"
+				+ "====================================================\n\n"
+				+ "Please press the number for the function you want and 'enter'\n"
+				+ "You'll be prompted to enter the needed parameters for each function.\n\n"
+				+ "1. calculateNGrams: Will ask for a text and a whole number n, and then will return all n-grams for\n"
+				+ "that text\n"
+				+ "2. mostFrequentNGram: Will ask for a text and a whole number n, and then will return only the most\n"
+				+ "frequent item of the n-grams for that text\n"
+				+ "3. Exit program";
 		System.out.println(menu);	//It's far more efficient to write on console once instead of doing it once per line
 	}
-	
+
 	/**
 	 * Error message if the selected option is not valid
 	 */
@@ -92,7 +162,16 @@ public class NGrams
 	{
 		System.out.println("--------------------------------\n"
 				+ "Your input was not valid. This program expects either 1, 2, or 3.\n"
+				+ "Returning to main menu.\n"
 				+ "--------------------------------\n\n");
 	}
-	
+
+	private static void printInvalidParams()
+	{
+		System.out.println("--------------------------------\n"
+				+ "Your input was not valid. This program expects whole numbers.\n"
+				+ "Returning to main menu.\n"
+				+ "--------------------------------\n\n");
+	}
+
 }
